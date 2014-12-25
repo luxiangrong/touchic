fd85849eee3e7ef92130f095ff5af212ec015f9e
luxiangrong <luxiangrongwave@gmail.com>
2014-12-25 17:00:18 +0800

    nav init

--- js/function.js
+++ js/function.js
@@ -1,7 +1,16 @@
 ;(function($) {
 	"use strict";
 	$(document).ready(function(){
-		
+		var scrollNav = {};
+		scrollNav.width = $(".scroll-nav").width();
+		scrollNav.count = $(".scroll-nav").find('.bg li').length;
+		$(".scroll-nav").find('li').width(scrollNav.width/scrollNav.count );
+		$(".scroll-nav ul.bg li").each(function(i, item){
+			$(item).css('left',  i * scrollNav.width/scrollNav.count + 'px');
+		});
+		$(".scroll-nav ul.fg li").each(function(i, item){
+			$(item).css('left',  i * scrollNav.width/scrollNav.count + 'px');
+		});
 	});
 })(jQuery);
 
